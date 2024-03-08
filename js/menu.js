c340a1704ab1cb3229056a209750b7562038b491
AkashLokhande <akashlok97@gmail.com>
2024-03-08 21:09:18 +0530

    changes

--- js/menu.js
+++ js/menu.js
@@ -7,8 +7,7 @@
           .then((response) => response.json())
           .then((menuItems) => {
             menuItems.menu.forEach((item) => {
-              document.querySelector('.loader-wrapper').style.display = 'none'+
-              9
+              document.querySelector('.loader-wrapper').style.display = 'none'
               const menuItemDiv = document.createElement("div");
               menuItemDiv.className = "menu-item";
               menuItemDiv.innerHTML = `
